03c4cf3d1b504cedd5acbd55e56991edbc459773
Banx87 <joni.lindholm@gmail.com>
2025-04-09 11:39:14 +0300

    [UPDATE] Refactor index.php to instantiate PagesRepository for page data retrieval

--- index.php
+++ index.php
@@ -4,9 +4,11 @@ require __DIR__ . '/inc/all.inc.php';
 
 $page = @(string) $_GET['page'] ?? 'index';
 
-
 if ($page === 'index') {
-    $pagesController = new \App\Frontend\Controller\PagesController();
+    $pagesRepository = new \App\Repository\PagesRepository($pdo);
+    // $pagesRepository->getPageBySlug($page);
+
+    $pagesController = new \App\Frontend\Controller\PagesController($pagesRepository);
     $pagesController->showPage('index');
 } else {
     $notFoundController = new \App\Frontend\Controller\NotFoundController();
