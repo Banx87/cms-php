b950d7df2cdf4601c2db12ffa4c5d3f0caeb450f
Banx87 <joni.lindholm@gmail.com>
2025-04-09 10:29:53 +0300

    Implement index page routing and add NotFoundController for 404 errors

--- index.php
+++ index.php
@@ -2,3 +2,13 @@
 
 require __DIR__ . '/inc/all.inc.php';
 
+$page = @(string) $_GET['page'] ?? 'index';
+
+
+if ($page === 'index') {
+    echo "TODO: Develop the index page";
+} else {
+    $notFoundController = new \App\Frontend\Controller\NotFoundController();
+    $notFoundController->error404();
+    exit;
+}
