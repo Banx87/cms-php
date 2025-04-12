a3bf92c1cb6b455e697abf0f8ad56a93666eb9b8
Banx87 <joni.lindholm@gmail.com>
2025-04-12 16:08:03 +0300

    [MOD] Enhance authentication handling in PagesAdminController routes

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ $container = new \App\Support\Container;
 $container->bind('pdo', function () {
     return require __DIR__ . '/inc/db-connect.inc.php';
 });
+$container->bind('authService', function () use ($container) {
+    $pdo = $container->get('pdo');
+    return new \App\Admin\Support\AuthService($pdo);
+});
 $container->bind('pagesRepository', function () use ($container) {
     $pdo = $container->get('pdo');
     return new \App\Repository\PagesRepository($pdo);
@@ -23,6 +27,10 @@ $container->bind('pagesAdminController', function () use ($container) {
     $pagesRepository = $container->get('pagesRepository');
     return new \App\Admin\Controller\PagesAdminController($pagesRepository);
 });
+$container->bind('loginController', function () use ($container) {
+    $authService = $container->get('authService');
+    return new \App\Admin\Controller\LoginController($authService);
+});
 
 $route = @(string) $_GET['route'] ?? 'pages';
 
@@ -32,15 +40,32 @@ if ($route === 'pages' || empty($route)) {
     $pagesController = $container->get('pagesController');
     $pagesController->showPage($page);
 } elseif ($route === 'admin/pages') {
+    $authService = $container->get('authService');
+    $authService->ensureLoggedIn();
     $pagesAdminController = $container->get('pagesAdminController');
     $pagesAdminController->index();
+} elseif ($route === 'admin/login') {
+    $loginController = $container->get('loginController');
+    $loginController->login();
+} elseif ($route === 'admin/logout') {
+    $loginController = $container->get('loginController');
+    $loginController->logout();
 } elseif ($route === 'admin/pages/create') {
+    $authService = $container->get('authService');
+    $authService->ensureLoggedIn();
+
     $pagesAdminController = $container->get('pagesAdminController');
     $pagesAdminController->create();
 } elseif ($route === 'admin/pages/edit') {
+    $authService = $container->get('authService');
+    $authService->ensureLoggedIn();
+
     $pagesAdminController = $container->get('pagesAdminController');
     $pagesAdminController->edit();
 } elseif ($route === 'admin/pages/delete') {
+    $authService = $container->get('authService');
+    $authService->ensureLoggedIn();
+
     $pagesAdminController = $container->get('pagesAdminController');
     $pagesAdminController->delete();
 } else {
