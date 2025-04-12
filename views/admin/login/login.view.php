b4ed3f3ea38e8ffd4bae6385d77dda31570b1bac
Banx87 <joni.lindholm@gmail.com>
2025-04-12 16:09:43 +0300

    [ADD] Create login view for user authentication

---
+++ views/admin/login/login.view.php
@@ -0,0 +1,23 @@
+<?php
+if (!empty($errors)): ?>
+    <ul>
+        <?php foreach ($errors as $error): ?>
+            <li><?php echo e($error) ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+
+<form action="index.php?<?php echo http_build_query(['route' => 'admin/login']); ?>" method="post">
+
+    <label for="login-username">Username:</label>
+    <input type="text" id="login-username" name="username"
+        value="<?php if (!empty($_POST['username'])) echo e($_POST['username']) ?>" required>
+
+    <br>
+
+    <label for="login-password">Password:</label>
+    <input type="password" id="login-password" name="password" required>
+
+    <br><br>
+    <input type="submit" value="Login">
+</form>
